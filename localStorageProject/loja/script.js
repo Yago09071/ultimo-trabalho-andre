f7def3b5a11f09e3ebdf560ebc2d20ec2cc7b562
Yago <113902880+Yago09071@users.noreply.github.com>
2024-03-20 07:57:45 -0300

    Add files via upload

---
+++ localStorageProject/loja/script.js
@@ -0,0 +1,42 @@
+let produtos;
+
+window.onload = function () {
+var storedUser = localStorage.getItem("usuario")
+var user = JSON.parse(storedUser)
+
+document.getElementById("user").textContent = user.name;
+document.getElementById("perfil").textContent = user.name;
+document.getElementById("idPerfil").textContent = user.id;
+document.getElementById("novaData").textContent = user.dataEntrada;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("../Dados/loja.json").then((Response) => Response.json()),then((data) => {
+        produtos = data;
+        const produtosContainer = document.getElementById("produto-container");
+
+        produtos.forEach((produto, index) => {
+            const card = document.createElement("div")
+            card.className = "card"
+            card.style.width = "18rem"
+            card.style.marginRight = "10px"
+
+            const imagem = document.createElement("img");
+            imagem.src = produto.imagem;
+            imagem.alt = produto.descricao;
+            imagem.className = "card-img-top"
+
+            const cardBody = document.createElement("div")
+            cardBody.className = "card-body"
+
+            const cardTitle = document.createElement("h5")
+            cardTitle.className = "card-title"
+            cardTitle.textContent = produto.descricao
+
+            const cardText = document.createElement("p")
+            cardText.className = "card-text"
+            cardText.textContent = produto.preco.toFixed(2)
+        });
+
+    }).cacth((error) => console.error("Erro ao carregar o arquivo JSON", error))
+})
